09078ee0aaeb595a03f1c30439dc7ca65c457c58
dianarojoudg <diana.rojo@udgvirtual.udg.mx>
2023-10-18 11:01:26 -0600

    Modificación background y menu - página principal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          CECMEX primera prueba react
-        </p>
+        <div className="menu">
+          <img src="url_de_tu_logo.png" alt="Logo de la empresa" />
+          <nav>
+            <ul>
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Cursos</a></li>
+              <li><a href="#">Suscripción</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="content-container">
+          <p className="legend">
+          "SOMOS UNA EMPRESA DEDICADA A LA CAPACITACIÓN EN MEDICINA COMPLEMENTARIA CON PRESENCIA EN TODO MEXICO Y LATAM"
+          </p>
+        </div>
       </header>
     </div>
   );
